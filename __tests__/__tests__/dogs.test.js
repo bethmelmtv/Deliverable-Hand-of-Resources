d0263dda90c60638a0ffb6130a2e6342e40576d5
Beth <bethmelmtv@gmail.com>
2022-06-16 15:31:41 -0700

    got create test to pass

--- __tests__/__tests__/dogs.test.js
+++ __tests__/__tests__/dogs.test.js
@@ -3,12 +3,9 @@ const setup = require('../../data/setup');
 const request = require('supertest');
 const app = require('../../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('testing the create route', () => {
   beforeEach(() => {
     return setup(pool);
-  });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
   });
 
   it('POST/dog should create a new dog', async () => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists dogs;
+
+CREATE table dogs (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  breed VARCHAR NOT NULL,
+  family VARCHAR NOT NULL
+);
+
+INSERT INTO dogs (name, breed, family) VALUES
+('Frankie', 'Pomeranian','Beths'),
+('Jeep','Labrador','Danis'),
+('Oscar','Poodle','Rebekahs');

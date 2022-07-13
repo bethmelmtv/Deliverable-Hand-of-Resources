5dae4124d1b46446bee181164212e3a26398a5b1
Beth <bethmelmtv@gmail.com>
2022-07-12 17:05:48 -0700

    added DELETE and POST tests to cities test folder

--- __tests__/cities.test.js
+++ __tests__/cities.test.js
@@ -9,13 +9,11 @@ describe('testing the read route', () => {
   });
 
   it('PUT/cities/:id cat should update a city', async () => {
-    const resp = await request(app)
-      .put('/cities/2')
-      .send({
-        city: 'Charleston',
-        state: 'North Carolina',
-        temperature: 'muggy',
-      });
+    const resp = await request(app).put('/cities/2').send({
+      city: 'Charleston',
+      state: 'North Carolina',
+      temperature: 'muggy',
+    });
     expect(resp.body.name).toEqual('Charleston');
   });
 
@@ -30,7 +28,7 @@ describe('testing the read route', () => {
     });
   });
 
-  it('/cities should return a list of cities', async () => {
+  it(' /cities should return a list of cities', async () => {
     const resp = await request(app).get('/cities');
     expect(resp.status).toEqual(200);
     expect(resp.body).toEqual([
@@ -53,6 +51,25 @@ describe('testing the read route', () => {
         temperature: 'dosent know what it wants',
       },
     ]);
+  });
+
+  it('POST/cities should create a new city', async () => {
+    const resp = await request(app).post('/cities').send({
+      city: 'Portlandia',
+      state: 'Oregon',
+      temperature: 'overcast',
+    });
+    expect(resp.body.city).toEqual('Portlandia');
+    expect(resp.body.state).toEqual('Oregon');
+    expect(resp.body.temperature).toEqual('overcast');
+  });
+
+  it('DELETE/cities/:id should delete a city', async () => {
+    const resp = await request(app).delete('/cities/2');
+    expect(resp.status).toEqual(200);
+    expect(resp.body.id).toEqual('2');
+    const { status } = await request(app).get('/cities/2');
+    expect(status).toEqual(404);
   });
 
   afterAll(() => {
